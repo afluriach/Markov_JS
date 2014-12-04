c7c9d1d334ee9ffc29c9c9b1fd3a1f6b9e7806de
ant <afluriach@gmail.com>
2014-12-04 08:26:24 -0500

    remove Class, set TrieNode prototype directly

--- public_html/markov.js
+++ public_html/markov.js
@@ -1,66 +1,5 @@
 //Electric Sun Studio Copyright (c) 2014
 
-//http://ejohn.org/blog/simple-javascript-inheritance/
-(function(){
-  var initializing = false, fnTest = /xyz/.test(function(){xyz;}) ? /\b_super\b/ : /.*/;
- 
-  // The base Class implementation (does nothing)
-  this.Class = function(){};
- 
-  // Create a new Class that inherits from this class
-  Class.extend = function(prop) {
-    var _super = this.prototype;
-   
-    // Instantiate a base class (but only create the instance,
-    // don't run the init constructor)
-    initializing = true;
-    var prototype = new this();
-    initializing = false;
-   
-    // Copy the properties over onto the new prototype
-    for (var name in prop) {
-      // Check if we're overwriting an existing function
-      prototype[name] = typeof prop[name] == "function" &&
-        typeof _super[name] == "function" && fnTest.test(prop[name]) ?
-        (function(name, fn){
-          return function() {
-            var tmp = this._super;
-           
-            // Add a new ._super() method that is the same method
-            // but on the super-class
-            this._super = _super[name];
-           
-            // The method only need to be bound temporarily, so we
-            // remove it when we're done executing
-            var ret = fn.apply(this, arguments);        
-            this._super = tmp;
-           
-            return ret;
-          };
-        })(name, prop[name]) :
-        prop[name];
-    }
-   
-    // The dummy class constructor
-    function Class() {
-      // All construction is actually done in the init method
-      if ( !initializing && this.ctor )
-        this.ctor.apply(this, arguments);
-    }
-   
-    // Populate our constructed prototype object
-    Class.prototype = prototype;
-   
-    // Enforce the constructor to be what we expect
-    Class.prototype.constructor = Class;
- 
-    // And make this class extendable
-    Class.extend = arguments.callee;
-   
-    return Class;
-  };
-})();
-
 //map each word to an ID
 var wordMap = {};
 //and each ID back to word
@@ -81,13 +20,14 @@ function getWordId(word)
     return wordMap[word];
 }
 
-TrieNode = Class.extend({
-    ctor: function()
-    {
-        this.next = {}
-        //if the sequence ends here, the result
-        this.end = null;
-    },
+TrieNode = function()
+{
+    this.next = {}
+    //if the sequence ends here, the result
+    this.end = null;
+};
+
+TrieNode.prototype =  {
     addSequence: function(seq, val)
     {
         if(seq.length === 0)
@@ -128,7 +68,7 @@ TrieNode = Class.extend({
         
         return this.next[prefix[idx]].getSuffix(prefix, idx+1);
     }
-});
+};
 
 trie = new TrieNode();
 
